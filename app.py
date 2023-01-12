f6ca1236f62e202a4cef6e87132b825aa8fab8d0
Anna Kazantseva <anna.y.kazantseva@gmail.com>
2023-01-12 12:25:28 +1100

    Add files via upload
    project uploaded

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+
+ 
+app = Flask(__name__)
+
+ 
+@app.route('/')
+@app.route('/index')
+def index():
+    name = request.args.get('username')
+    return  render_template('index.html', name='Marci Fumons')
+
